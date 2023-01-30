6383130b9952b70d4fcb361835c061e75922c2ff
ritty27 <jspark9812@gmail.com>
2023-01-30 18:17:51 +0900

    feat: 락과 데이터 변경 트랜잭션 분리

--- src/main/kotlin/com/ritty27/dblock/service/RedisLockAccountService.kt
+++ src/main/kotlin/com/ritty27/dblock/service/RedisLockAccountService.kt
@@ -4,6 +4,7 @@ import com.ritty27.dblock.entity.AccountEntity
 import com.ritty27.dblock.repository.AccountRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class RedisLockAccountService(
@@ -16,27 +17,29 @@ class RedisLockAccountService(
         const val LOCK_RE_REQUEST_WAITE_TIME = 100
     }
 
-    /**
-     * TODO
-     * 1. Transaction 사용하지 않을 때만 성공
-     * 2. Transaction 걸었을 때
-     *      1) Redis Transaction 사용 -> Redis 는 Write Command 를 Transaction 종료 시점에 한 번에
-     *      사용하기 때문에, Lock 을 얻을 수 없다. (테스트 하기 위해서는 Test 에서 Import RedisConfig 주석 제거 필요)
-     *      2) Redis Transaction 미사용 -> Redis lock release 이후 DB commit 이 진행되어
-     *      그 사이에 다른 곳에서 Lock 획득, 값 변경 할 수 있다. -> TEST 실패한다.
-     */
-//    @Transactional
+    // FACADE
     fun decrease(id: Long, fare: Long): AccountEntity {
+        // GET LOCK
         while (!redisLockProvider.getLock(id)) {
             Thread.sleep(LOCK_RE_REQUEST_WAITE_TIME.toLong())
         }
+
+        // DATA FIX
+        val account = decreaseFare(id, fare)
+
+        // RELEASE LOCK
+        redisLockProvider.releaseLock(id)
+
+        return account
+    }
+
+    @Transactional
+    fun decreaseFare(id: Long, fare: Long): AccountEntity {
 
         val account = accountRepository.findByIdOrNull(id) ?: throw RuntimeException()
         account.decreaseMoney(fare)
 
         accountRepository.save(account)
-
-        redisLockProvider.releaseLock(id)
 
         return account
     }
